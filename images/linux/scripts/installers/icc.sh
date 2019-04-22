5e6a141354c74718ece00ca1bfc8f541e6de21c2
triskadecaepyon <dcltechnology@gmail.com>
2019-04-22 15:34:20 -0500

    initial linux icc test

---
+++ images/linux/scripts/installers/icc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+################################################################################
+##  File:  icc.sh
+##  Team:  CI-Platform
+##  Desc:  Installs ICC for C/C++
+################################################################################
+
+# Source the helpers for use with the script
+source $HELPER_SCRIPTS/document.sh
+
+apt-get update -qq
+apt-get install -y -qq cmake cpio g++ openssl wget
+
+product_year=2019
+update_number=3
+irc_id=15272
+url=http://registrationcenter-download.intel.com/akdlm/irc_nas/tec/${irc_id}
+installer=parallel_studio_xe_${product_year}_update${update_number}_composer_edition_online
+ext=tgz
+license_dir=/opt/intel/licenses
+
+if [ -z "${IS_LICENSE_DATA_LINUX}" ]
+then
+    echo "PSXE license found, downloading icc"
+    wget -q $url/$installer.$ext
+    [ $? != 0 ] && echo $0: Error, download of $url/$installer.$ext failed && exit 1
+    tar zxf $installer.$ext
+    mkdir -p $license_dir
+    [ -z "${IS_LICENSE_DATA_LINUX}" ] && echo $0: Error, set IS_LICENSE_DATA_LINUX and try again && exit 1
+    printenv IS_LICENSE_DATA_LINUX | openssl base64 -d -A > $license_dir/license.lic
+    $installer/install.sh --silent images/linux/config/helpers/icc_silent.cfg
+else
+    echo "No PSXE license found, skipping icc install"
+fi
